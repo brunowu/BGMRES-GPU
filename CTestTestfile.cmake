0c674ba003386528b4a85af2a220cdc944251602
brunowu <xinzhe.wu1990@gmail.com>
2018-10-11 20:46:13 +0200

    node type set to serial, because no cuda version implementation of trilinos now

---
+++ CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /Users/xwu/phd_xwu/BGMRES-GPU
+# Build directory: /Users/xwu/phd_xwu/BGMRES-GPU
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
